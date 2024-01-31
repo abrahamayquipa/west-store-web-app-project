3da6671e3a2ef8f52f7e4d5adae31268c6143ee1
abrahamayquipa <abrahamayquipa2000@gmail.com>
2024-01-31 17:11:28 -0500

    feat: add the entire project structure

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.querySelector('.root') ?? document.createElement('div')
+const root = createRoot(rootElement)
+
+root.render(
+  <>
+    
+  </>
+);
