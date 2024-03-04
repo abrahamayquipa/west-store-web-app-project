a8c72cdf19d5dddf47c5608a1f93e38c48e0238d
abrahamayquipa <abrahamayquipa2000@gmail.com>
2024-03-04 11:03:23 -0500

    feat: fix the index.js file

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,10 @@ import productsRouter from './product.mjs';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://west-co.vercel.app'
 }));
 
 app.use('/api', productsRouter);
-
 
 app.listen(3000, () => {
   console.log('Server on 3000 port');
